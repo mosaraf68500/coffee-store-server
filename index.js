56d4a29acfd859b599fb42d5cf8b2204779e2921
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-05-15 01:49:16 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
-const express = require('express');
-const cors = require('cors');
-const app=express();
-const port=process.env.PORT ||3000;
-
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send('coffee store server is running');
-})
+const uri = `mongodb+srv://${process.env.COFFEE_USER}:${process.env.COFFEE_PASS}@cluster0.bvkgo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-app.listen(port,()=>{
-    console.log(`Coffe store server is running on port:${port}`)
-})
+async function run() {
+  try {
+    
+    await client.connect();
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("coffee store server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Coffe store server is running on port:${port}`);
+});
